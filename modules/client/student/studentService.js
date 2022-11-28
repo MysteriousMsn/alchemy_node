873e8cb50aae0dce94929086f7b2e2bf3e7c7ead
Mohsin Sunasara <mohsin.sunasara@openxcellinc.com>
2022-11-28 10:22:17 +0530

    updated readme

--- modules/client/student/studentService.js
+++ modules/client/student/studentService.js
@@ -14,24 +14,5 @@ studentService.createStudent = (data) => {
 studentService.createBulkCourses = (data, options) => {
   return studentCoursesModel.bulkCreate(data, options);
 };
-studentService.findAdminByEmail = (email) => {
-  let query = {
-    where: {
-      email: email,
-    },
-  };
-  return adminModel.findOne(query);
-};
-studentService.createProgram = (data) => {
-  return programLibraryModel.create(data);
-};
-
-studentService.getAllPrograms = (query) => {
-  return programLibraryModel.findAll(query);
-};
-
-studentService.updatePassword = (data, query) => {
-  return adminModel.update(data, query);
-};
 
 module.exports = studentService;
